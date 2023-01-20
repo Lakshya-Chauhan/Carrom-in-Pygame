77374a3cc08231062b967ed91afba424c29da3f2
Legendary <99966559+Legendary3995@users.noreply.github.com>
2023-01-20 17:39:52 +0530

    Update collision.py

--- collision.py
+++ collision.py
@@ -7,13 +7,15 @@ from math import inf as infinity
 frameRate = 200
 collisions = []
 dt = 1/200
+
+
 class obj:
-    boardFramesDist = [47, 46]
-    stoppedObjs = 0
-    screen_size = [800, 800]
-    def __init__(self, mass, pos, radius, color = (0, 0, 0), acceleration = 0, velx = 0, vely = 0, e = 1, number = None):
+    boardFramesDist = [47, 46+100]
+    screen_size = [800, 800+200]
+
+    def __init__(self, mass, pos, radius, color=(0, 0, 0), acceleration=0, velx=0, vely=0, e=1, number=None):
         self.mass = mass
-        self.pos = pygame.Vector2(pos[0],pos[1])
+        self.pos = pygame.Vector2(pos[0], pos[1])
         self.radius = radius
         self.color = color
         self.x = velx
@@ -23,13 +25,14 @@ class obj:
         self.e = e
         self.number = number
         self.stop = None
-    
+
     def display(self, SURFACE):
         try:
-            pygame.draw.circle(SURFACE, self.color, (round(self.pos[0]),round(self.pos[1])), self.radius)
+            pygame.draw.circle(SURFACE, self.color, (round(
+                self.pos[0]), round(self.pos[1])), self.radius)
         except:
             print(self.color)
-    
+
     def update(self, dt):
         if abs(self.x) > 10000:
             self.x = (abs(self.x)/(self.x))*1000
@@ -38,20 +41,20 @@ class obj:
         if self.stop != True:
             self.pos += self.vel*dt
 
-            self.vel -= self.friction(9.8, 0.3)*dt
+            self.vel -= self.friction(9.81, 35*dt)
             self.x = self.vel[0]
             self.y = self.vel[1]
-            if self.vel.magnitude() < 0.1:
-                obj.stoppedObjs += 1
-            if self.pos[0] >  obj.screen_size[0]-self.radius-obj.boardFramesDist[0]:
-                self.pos[0] -= 2*(obj.boardFramesDist[0] + self.pos[0]-self.screen_size[0]+self.radius)
+            if self.pos[0] > obj.screen_size[0]-self.radius-obj.boardFramesDist[0]:
+                self.pos[0] -= 2*(obj.boardFramesDist[0] +
+                                  self.pos[0]-self.screen_size[0]+self.radius)
                 self.vel = pygame.Vector2(self.x, self.y)
                 self.vel *= 0.9
                 self.x = self.vel[0]
                 self.y = self.vel[1]
                 self.x *= -1
             elif self.pos[0] < self.radius + obj.boardFramesDist[0]:
-                self.pos[0] += 2*(obj.boardFramesDist[0] + self.radius-self.pos[0])
+                self.pos[0] += 2*(obj.boardFramesDist[0] +
+                                  self.radius-self.pos[0])
                 self.vel = pygame.Vector2(self.x, self.y)
                 self.vel *= 0.9
                 self.x = self.vel[0]
@@ -59,50 +62,62 @@ class obj:
                 self.x *= -1
 
             if self.pos[1] > obj.screen_size[1]-self.radius-obj.boardFramesDist[1]:
-                self.pos[1] -= 2*(self.pos[1]-self.screen_size[1]+self.radius+obj.boardFramesDist[1])
+                self.pos[1] -= 2*(self.pos[1]-self.screen_size[1] +
+                                  self.radius+obj.boardFramesDist[1])
                 self.vel = pygame.Vector2(self.x, self.y)
                 self.vel *= 0.9
                 self.x = self.vel[0]
                 self.y = self.vel[1]
                 self.y *= -1
             elif self.pos[1] < self.radius + obj.boardFramesDist[1]:
-                self.pos[1] += 2*(self.radius-self.pos[1] + obj.boardFramesDist[1])
+                self.pos[1] += 2*(self.radius-self.pos[1] +
+                                  obj.boardFramesDist[1])
                 self.vel = pygame.Vector2(self.x, self.y)
                 self.vel *= 0.9
                 self.x = self.vel[0]
                 self.y = self.vel[1]
                 self.y *= -1
-            self.vel = pygame.Vector2(self.x,self.y)
+            self.vel = pygame.Vector2(self.x, self.y)
             if self.stop == False:
                 self.stop = None
         else:
             self.stop = False
-            
 
     def friction(self, gravity, mu):
-        fric = pygame.Vector2(min(gravity*mu*self.mass, self.vel.magnitude()))
+        fric = pygame.Vector2(min(gravity*mu, self.vel.magnitude()))
         return fric.rotate(fric.angle_to(self.vel))
-    
-    def collision(self, group:list, collisions:list, dt):
+
+    def collision(self, group: list, collisions: list, dt):
         for i in group:
             if [self.number, i.number] not in collisions:
                 if (i.pos[0] - self.pos[0]) <= i.radius + self.radius:
                     if (i.pos[1] - self.pos[1]) <= i.radius + self.radius:
                         if distance(i.pos, self.pos) <= i.radius + self.radius:
-                            
-                            self.vel, i.vel = self.vel - ((1+i.e)*i.mass/(self.mass+i.mass)) * pygame.math.Vector2.project((self.vel - i.vel), (self.pos - i.pos)), i.vel - ((1+self.e)*self.mass/(self.mass+i.mass)) * pygame.math.Vector2.project((i.vel - self.vel), (i.pos - self.pos))
-                            
+                            if i.vel.magnitude() == 0:
+                                self.vel, i.vel = self.vel - ((1+i.e)*i.mass/(self.mass+i.mass)) * pygame.math.Vector2.project((self.vel - i.vel), (self.pos - i.pos)), i.vel - (
+                                    (1+self.e)*self.mass/(self.mass+i.mass)) * pygame.math.Vector2.project((i.vel - self.vel), (i.pos - self.pos))
+                                # i.vel -= i.friction(1, 100*dt)
+                                i.x = i.vel[0]
+                                i.y = i.vel[1]
+                            elif self.vel.magnitude() == 0:
+                                self.vel, i.vel = self.vel - ((1+i.e)*i.mass/(self.mass+i.mass)) * pygame.math.Vector2.project((self.vel - i.vel), (self.pos - i.pos)), i.vel - (
+                                    (1+self.e)*self.mass/(self.mass+i.mass)) * pygame.math.Vector2.project((i.vel - self.vel), (i.pos - self.pos))
+                                # self.vel -= self.friction(1, 100*dt)
+                                self.x = self.vel[0]
+                                self.y = self.vel[1]
+                            else:
+                                self.vel, i.vel = self.vel - ((1+i.e)*i.mass/(self.mass+i.mass)) * pygame.math.Vector2.project((self.vel - i.vel), (self.pos - i.pos)), i.vel - (
+                                    (1+self.e)*self.mass/(self.mass+i.mass)) * pygame.math.Vector2.project((i.vel - self.vel), (i.pos - self.pos))
                             self.x = self.vel[0]
                             self.y = self.vel[1]
                             i.x = i.vel[0]
                             i.y = i.vel[1]
 
-
                             # self.color = [abs(self.color[0]+ (0.5-random.random())*20)%256, abs(self.color[1]+ (0.5-random.random())*20)%256, abs(self.color[2]+ (0.5-random.random())*20)%256]
                             # i.color = [abs(i.color[0]+ (0.5-random.random())*20)%256, abs(i.color[1]+ (0.5-random.random())*20)%256, abs(i.color[2]+ (0.5-random.random())*20)%256]
 
-                            self.update(dt *2)
-                            i.update(dt *2)
+                            self.update(dt * 2)
+                            i.update(dt * 2)
 
                             if self.stop == None:
                                 self.stop = True
@@ -112,51 +127,77 @@ class obj:
                             dist = distance(i.pos, self.pos)
                             if dist < i.radius + self.radius:
                                 if i.pos[0]-self.pos[0] == 0:
-                                    i.pos[1] += sign(i.pos[1] - self.pos[1]) * abs(i.radius+self.radius-dist)/2
-                                    self.pos[1] += sign(self.pos[1] - i.pos[1]) * abs(i.radius+self.radius-dist)/2
+                                    i.pos[1] += sign(i.pos[1] - self.pos[1]) * \
+                                        abs(i.radius+self.radius-dist)/2
+                                    self.pos[1] += sign(self.pos[1] - i.pos[1]) * \
+                                        abs(i.radius+self.radius-dist)/2
                                 elif i.pos[1]-self.pos[1] == 0:
-                                    i.pos[0] += sign(i.pos[0] - self.pos[0]) * abs(i.radius+self.radius-dist)/2
-                                    self.pos[0] += sign(self.pos[0] - i.pos[0]) * abs(i.radius+self.radius-dist)/2
+                                    i.pos[0] += sign(i.pos[0] - self.pos[0]) * \
+                                        abs(i.radius+self.radius-dist)/2
+                                    self.pos[0] += sign(self.pos[0] - i.pos[0]) * \
+                                        abs(i.radius+self.radius-dist)/2
                                 else:
                                     incHyp = (i.radius+self.radius-dist)
-                                    incx = (incHyp/dist)*(abs(i.pos[0] - self.pos[0]))
-                                    incy = (incHyp/dist)*(abs(i.pos[1] - self.pos[1]))
-                                    i.pos[0] += incx* sign(i.pos[0] - self.pos[0])*((i.mass*i.radius)/(self.mass*self.radius+ i.mass*i.radius))
-                                    i.pos[1] += incy *sign(i.pos[1] - self.pos[1])*((i.mass*i.radius)/(self.mass*self.radius+ i.mass*i.radius))
-                                    self.pos[0] += incx* sign(self.pos[0] - i.pos[0])*((self.mass*self.radius)/(self.mass*self.radius+ i.mass*i.radius))
-                                    self.pos[1] += incy *sign(self.pos[1] - i.pos[1])*((self.mass*self.radius)/(self.mass*self.radius+ i.mass*i.radius))
+                                    incx = (incHyp/dist) * \
+                                        (abs(i.pos[0] - self.pos[0]))
+                                    incy = (incHyp/dist) * \
+                                        (abs(i.pos[1] - self.pos[1]))
+                                    i.pos[0] += incx * sign(i.pos[0] - self.pos[0])*(
+                                        (i.mass*i.radius)/(self.mass*self.radius + i.mass*i.radius))
+                                    i.pos[1] += incy * sign(i.pos[1] - self.pos[1])*(
+                                        (i.mass*i.radius)/(self.mass*self.radius + i.mass*i.radius))
+                                    self.pos[0] += incx * sign(self.pos[0] - i.pos[0])*(
+                                        (self.mass*self.radius)/(self.mass*self.radius + i.mass*i.radius))
+                                    self.pos[1] += incy * sign(self.pos[1] - i.pos[1])*(
+                                        (self.mass*self.radius)/(self.mass*self.radius + i.mass*i.radius))
                                     # print(f"Resolved Another bug!!! {[i.number, self.number]}")
                             return [[self.number, i.number], [i.number, self.number]]
         return []
 
+
 def sign(num):
     return 1 if num > 0 else -1
+
+
+def stopped(List):
+    stoppedObjs = 0
+    for i in List:
+        if i.vel.magnitude() == 0:
+            stoppedObjs += 1
+    return stoppedObjs
 
 def exchange_vel(ivel1, ivel2, m1, m2, e1, e2):
     fvel1 = (1/(m1+m2))*((ivel1*(m1-e1*m2)) + (ivel2*(1+e1)*m2))
     fvel2 = (1/(m1+m2))*((ivel2*(m2-e2*m1)) + (ivel1*(1+e2)*m1))
-    return [fvel1,fvel2]
-                
+    return [fvel1, fvel2]
 
-def distance(point1,point2):
+
+def distance(point1, point2):
     return (((point1[0]-point2[0])**2) + ((point1[1]-point2[1])**2))**0.5
 
 
 if __name__ == '__main__':
     balls = []
     for i in range(0):
-        balls.append(obj(1+int(random.random()*-1), (40 + int(random.random()*600),40 + int(random.random()*600)), 10, (int(random.random()*200)+56,int(random.random()*200)+56,int(random.random()*200)+56), 0, int((0.5-random.random())*5000), int((0.5-random.random())*5000), 0.75, i))
+        balls.append(obj(1+int(random.random()*-1), (40 + int(random.random()*600), 40 + int(random.random()*600)), 10, (int(random.random()*200)+56,
+                     int(random.random()*200)+56, int(random.random()*200)+56), 0, int((0.5-random.random())*5000), int((0.5-random.random())*5000), 0.75, i))
     # def __init__(self, mass, pos, radius, color = (0, 0, 0), acceleration = 0, velx = 0, vely = 0, e = 1, number = None):
-    balls.append(obj(28, (400, 200), 20.6, (0,0,0), 0, 0*5, 800*5, 0.7, 1000))
-    balls.append(obj(10, (410, 300), 15.1, (0,0,0), 0, 0*5, 100*5, 0.5, 1001))
-    balls.append(obj(10, (420, 400), 15.1, (0,0,0), 0, 0*5, 100*5, 0.5, 1002))
-    balls.append(obj(10, (430, 500), 15.1, (0,0,0), 0, 0*5, 100*5, 0.5, 1003))
-    balls.append(obj(10, (440, 600), 15.1, (0,0,0), 0, 0*5, 100*5, 0.5, 1004))
-    balls.append(obj(10, (450, 700), 15.1, (0,0,0), 0, 0*5, 100*5, 0.5, 1005))
+    balls.append(obj(28, (400, 200), 20.6, (0, 0, 0),
+                 0, 0*5, 800*5, 0.7, 1000))
+    balls.append(obj(10, (410, 300), 15.1, (0, 0, 0),
+                 0, 0*5, 100*5, 0.5, 1001))
+    balls.append(obj(10, (420, 400), 15.1, (0, 0, 0),
+                 0, 0*5, 100*5, 0.5, 1002))
+    balls.append(obj(10, (430, 500), 15.1, (0, 0, 0),
+                 0, 0*5, 100*5, 0.5, 1003))
+    balls.append(obj(10, (440, 600), 15.1, (0, 0, 0),
+                 0, 0*5, 100*5, 0.5, 1004))
+    balls.append(obj(10, (450, 700), 15.1, (0, 0, 0),
+                 0, 0*5, 100*5, 0.5, 1005))
     # balls.append(obj(1000, (600, 400), 30, (0,0,0), 0, 5*5, 5*5, 1, 1002))
-    
+
     pygame.init()
-    screen = pygame.display.set_mode((800,800))
+    screen = pygame.display.set_mode((800, 800))
     pygame.display.set_caption("Collision")
     running = True
     initTime = time.time()
@@ -168,20 +209,23 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 running = False
 
-        #code
-        screen.fill((150,150,150))
+        # code
+        screen.fill((150, 150, 150))
         for i in balls:
-            collisions.extend(i.collision([objs for objs in balls if objs.number != i.number], collisions, dt))
+            collisions.extend(i.collision(
+                [objs for objs in balls if objs.number != i.number], collisions, dt))
             i.update(dt, screen)
             i.display()
         if obj.stoppedObjs == len(balls):
             obj.stoppedObjs = 0
             for i in balls:
-                i.vel = pygame.Vector2(0,0)
+                i.vel = pygame.Vector2(0, 0)
             print("All stop")
         pygame.display.update()
         endTime = time.time()
         dt = endTime-initTime
         initTime = endTime
-        if dt != 0: frameRate = 1/dt
-        else: frameRate = 1000
+        if dt != 0:
+            frameRate = 1/dt
+        else:
+            frameRate = 1000
